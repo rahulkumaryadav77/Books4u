0b80d7c283db03d0fb43506b0a2f5e60484b15d0
Glitch (cheerful-fearless-train) <none>
2021-08-10 09:49:29 +0000

    ☃️🌁 Checkpoint ./views/catBooks.ejs:34220414/67 ./controllers/index.js:34220414/52 ./views/blogs/showBlog.ejs:34220414/80

--- controllers/index.js
+++ controllers/index.js
@@ -6,9 +6,9 @@ const Category = require('../models/categoriesbook');
 module.exports = {
     async homePage(req, res) {
         const categories = await Category.find({});
-        const books = await Books.aggregate([{$limit: 8}]);
+        const books = await Books.aggregate([{$limit: 8}, { $sort : { _id : -1 } }]);
         const peoples = await People.aggregate([{$limit: 12}]);
-        const blogs = await AdminBlog.aggregate([{$limit: 4}]);
+        const blogs = await AdminBlog.aggregate([{$limit: 4}, { $sort : { _id : -1 } }]);
         res.render('home', { title: 'Readers Club', books, peoples, categories, blogs });
     }
 }
